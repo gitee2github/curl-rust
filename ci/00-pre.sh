c96f39b2e3859718f4bf2c9ff5cc3c09e0347d8f
zzy <1377519372@qq.com>
2022-08-31 11:48:44 +0800

    add make to pre-enviroment

--- ci/00-pre.sh
+++ ci/00-pre.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum clean all
-sudo yum install -y gcc openssl-libs automake
+sudo yum install -y gcc openssl-libs automake make cmake
 
 #git加速并安装rust工具链
 git config --global url."https://github.91chi.fun/https://github.com/".insteadOf "https://github.com/"
