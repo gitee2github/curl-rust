2700f420f6c740fff81a6537e2f6c9a054fc6fc7
zzy <1377519372@qq.com>
2022-09-05 15:09:43 +0800

    upload performance test and LOC count script

---
+++ ci/04-count.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# echo "C files: lib/ftp*"
+# cloc ../lib/ftp*
+
+# echo "C files: lib/http*"
+# cloc ../lib/http* 
+
+# echo "C files: lib/vtls/"
+# cloc../lib/vtls/
+
+# echo "Rust ffi files: "
+# cloc ../rust/rust_ffi/*.rs ../rust/rust_ffi/src/
+
+# echo "Rust project files: "
+# cloc ../rust/rust_project/*.rs ../rust/rust_project/src/
