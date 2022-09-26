713bcbd74613b6c7668f51c9afaa49620bd932c6
wyf <763257820@qq.com>
2022-09-26 14:46:26 +0800

    add install nghttp2

--- ci/00-pre.sh
+++ ci/00-pre.sh
@@ -2,6 +2,11 @@
 sudo yum clean all
 sudo yum install -y gcc openssl-libs automake make cmake
 
+# nghttp2
+git clone https://github.com/tatsuhiro-t/nghttp2.git
+cd nghttp2 && autoreconf -i && automake && autoconf
+./configure && make && sudo make install
+
 #git加速并安装rust工具链
 git config --global url."https://github.91chi.fun/https://github.com/".insteadOf "https://github.com/"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o rustlang.sh
